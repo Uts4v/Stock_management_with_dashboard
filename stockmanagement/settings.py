e1080f47c2247633f884c8f87d512d0ad7aa1ced
uts4v <utsavstha033@gmail.com>
2026-06-13 14:45:55 +0545

    Secure SECRET_KEY via environment variable

--- stockmanagement/settings.py
+++ stockmanagement/settings.py
@@ -19,12 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7gl$0141q*+(z!)4jg3*4@gi#hido*y+30*%m)i2)ht=92^zq4'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7gl$0141q*+(z!)4jg3*4@gi#hido*y+30*%m)i2)ht=92^zq4')
 
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
